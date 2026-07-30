36bb04baa5287c33f7e2ab380ca3334a030a35cf
Henry Chinedu <henrychinedu377@gmail.com>
2026-07-30 10:31:57 +0100

    Initial portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+// Welcome message
+console.log("Welcome to Henry Chinedu's Portfolio!");
+
+// Show a message when the button is clicked
+const button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+    alert("My projects are coming soon! Stay tuned. 🚀");
+});
